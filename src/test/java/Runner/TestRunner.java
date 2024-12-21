cfb80f8be1b70a269c8cb5b7bb3716fc63ea9caf
Sesha Sravani <sraavz.aish@gmail.com>
2024-12-21 01:30:36 -0500

    runner & tree,graph

--- src/test/java/Runner/TestRunner.java
+++ src/test/java/Runner/TestRunner.java
@@ -1,6 +1,24 @@
-package Runner;
+package runner;
 
-public interface TestRunner 
-{
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.DataProvider;
 
+@CucumberOptions(
+        features = "src/test/resources/features", // Path to your feature files
+        glue = "StepDefinitions", // Path to your step definition classes
+        monochrome = true, // Makes the console output more readable
+        plugin = {
+                "pretty", // Pretty print for console output
+                "html:target/cucumber-reports/cucumber.html", // HTML report
+                "json:target/cucumber-reports/cucumber.json"  // JSON report
+        }
+)
+public class TestRunner extends AbstractTestNGCucumberTests {
+
+    @Override
+    @DataProvider(parallel = true) // Run tests in parallel
+    public Object[][] scenarios() {
+        return super.scenarios(); // Get the scenarios from Cucumber
+    }
 }
