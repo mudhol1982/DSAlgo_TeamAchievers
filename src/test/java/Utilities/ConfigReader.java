e59d109cf2c12453342371d611a46939162cdb76
Sesha Sravani <sraavz.aish@gmail.com>
2024-12-22 16:42:38 -0500

    added config reader & config props

--- src/test/java/Utilities/ConfigReader.java
+++ src/test/java/Utilities/ConfigReader.java
@@ -1,5 +1,37 @@
 package Utilities;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class ConfigReader {
 
+    private static Properties properties = new Properties();
+
+    // Load the config.properties file from the classpath
+    public static void loadConfig() throws IOException {
+        // Using ClassLoader to load the file from the classpath
+        InputStream inputStream = ConfigReader.class.getClassLoader().getResourceAsStream("Config/config.properties");
+
+        // If the file is not found, throw an exception
+        if (inputStream == null) {
+            throw new IOException("Config file not found in classpath");
+        }
+
+        // Load the properties from the file
+        properties.load(inputStream);
+    }
+
+    // Get the browser type from the config file
+    public static String getBrowserType() {
+        return properties.getProperty("browser");
+    }
+
+    // Get the URL from the config file
+    public static String getUrl() {
+        return properties.getProperty("url");
+    }
+
+   
 }
+
