38ab391823924491414da7df6d3c7867b0d3ef8b
learnersteen <learnersteen@gmail.com>
2024-12-27 12:49:33 -0500

    commit changes for HomePom page/Object

--- src/test/java/PageObjectModel/HomePom.java
+++ src/test/java/PageObjectModel/HomePom.java
@@ -1,5 +1,39 @@
 package PageObjectModel;
 
-public class HomePom {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import DriverManager.Driver_SetUp;
+import Utilities.ConfigReader;
 
+
+public class HomePom {
+	
+	public WebDriver driver = Driver_SetUp.getDriver();
+	
+	public By getStartedButton = By.cssSelector("button.btn");
+	public By contentHeader = By.cssSelector(".content h1");
+	public By contentText = By.cssSelector("div.content p");
+	
+    public void openURL() {
+    	
+    	driver.get(ConfigReader.getUrl());
+    	   	
+    }
+    
+    public void ClickHomePageGetStartedButton() {
+    	
+    	driver.findElement(getStartedButton).click();
+    }
+    
+	public String getTextForElement(By locator) {
+		String elementText = driver.findElement(locator).getText();
+		return elementText;
+	}
+
+	
 }
+	
+	
+	
+
+
