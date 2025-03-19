9f296fd53b59632182c5a8beb1c078c176f4700d
Sesha Sravani <sraavz.aish@gmail.com>
2025-03-19 17:11:43 -0400

    parallel & cross browser testing

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -2,15 +2,20 @@ package runner;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+
 
 @CucumberOptions(
         features = "src/test/resources/features", // Path to your feature files
         //glue = "StepDefinitions", // Path to your step definition classes
-        glue = {"StepDefinitions"}, // Package containing step definitions
+        glue = {"StepDefinitions", "AppHooks"}, // Package containing step definitions
         //glue = {"com.yourproject.StepDefinitions"},
         monochrome = true, // Makes the console output more readable
-        dryRun=true,
+        dryRun=false,
         plugin = {
                 "pretty", // Pretty print for console output
                 "html:target/cucumber-reports/cucumber.html", // HTML report
@@ -24,4 +29,8 @@ public class TestRunner extends AbstractTestNGCucumberTests {
     public Object[][] scenarios() {
         return super.scenarios(); // Get the scenarios from Cucumber
     }
-}
+    
+   
+
+	}
+    
